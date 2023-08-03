705a8810b6c59cbdf538d69eaa42599fa88710c1
pacmanlolo <michael.omotosho7@gmail.com>
2023-08-03 05:32:41 -0700

    Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -4,8 +4,11 @@ web application must be listening on 0.0.0.0, port 5000
 Routes:
 /: display “Hello HBNB!”
 /hbnb: display “HBNB”
-/c/<text>: display “C ” followed by the value of the text variable
+/c/<text>: display “C ”, followed by the value of the text variable
 (replace underscore _ symbols with a space )
+/python/(<text>): display “Python ”, followed by the value of the text
+variable (replace underscore _ symbols with a space )
+The default value of text is “is cool”
 You must use the option strict_slashes=False in your route definition
 """
 from flask import Flask
@@ -30,6 +33,14 @@ def c(text):
     """Displays "C" followed by the value of <text>."""
     text = text.replace("_", " ")
     return "C {}".format(text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Displays "Python" followed by the value of <text>."""
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
 
 
 if __name__ == "__main__":
